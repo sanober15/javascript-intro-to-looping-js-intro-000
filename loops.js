1c5108f304790e3d6714bd47fa6309dd9cf52d9a
sanober riaz <sanober@sanober.me>
2018-01-05 06:42:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function doWhileLoop(array){
   function maybeTrue() {
-  return Math.random() >= 0.5
+  return array.pop()
+  
 }
 
 do {
   console.log('doo-bee-doo-bee-doo')
-  array.pop()
-  array
+  
 } while (maybeTrue() && array.length > 0);
 array;
 }
