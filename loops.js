f4afa9a4aab84004c31b618f8c87491f635fd1ae
sanober riaz <sanober@sanober.me>
2018-01-05 06:47:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,3 +10,13 @@ do {
 } while (maybeTrue() && array.length > 0);
 array;
 }
+
+
+function forLoop(array){
+  for (var i = 0; i < 25; i++) {
+    var s = `I am ${i} stange loop`
+    array = [...array, s]
+    array
+    }
+    return array;
+  }
